846c64cd4415734d2da848f9754b348d345935d7
Ahmad Sofyan .A <sofyan7273@gmail.com>
2022-01-02 19:04:21 +0700

    create_description_book

--- flutter_app/lib/screens/selected_book_screen.dart
+++ flutter_app/lib/screens/selected_book_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/models/popularbook_model.dart';
 
 class SelectedBookScreen extends StatelessWidget {
+  SelectedBookScreen({
+    Key? key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,9 +18,98 @@ class SelectedBookScreen extends StatelessWidget {
                 expandedHeight: MediaQuery.of(context).size.height * 0.5,
                 flexibleSpace: Container(
                   height: MediaQuery.of(context).size.height * 0.5,
-                  color: Color(),
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned(
+                        left: 25,
+                        top: 35,
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: Container(
+                            width: 32,
+                            height: 32,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: Color(0xFF000000)),
+                            child: Image(
+                                image: AssetImage("assets/img/background.png")),
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Container(
+                          margin: EdgeInsets.only(bottom: 62),
+                          width: 225,
+                          height: 172,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                  image: AssetImage(
+                                      'assets/image/background.png'))),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              )
+              ),
+              SliverList(
+                  delegate: SliverChildListDelegate([
+                Padding(
+                  padding: EdgeInsets.only(top: 24, left: 25),
+                  child: Text(
+                    'Tittle',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 27,
+                        color: Colors.black87),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 7, left: 25),
+                  child: Text(
+                    'Author',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                        color: Colors.black38),
+                  ),
+                ),
+                Container(
+                  height: 28,
+                  margin: EdgeInsets.only(top: 23, bottom: 36),
+                  padding: EdgeInsets.only(left: 25),
+                  child: DefaultTabController(
+                    length: 3,
+                    child: TabBar(
+                        labelPadding: EdgeInsets.all(0),
+                        indicatorPadding: EdgeInsets.all(0),
+                        isScrollable: true,
+                        labelColor: Color(0xFF1A237E),
+                        unselectedLabelColor: Colors.lightBlue[100],
+                        tabs: [
+                          Tab(
+                            child: Container(
+                              margin: EdgeInsets.only(right: 23),
+                              child: Text('Description'),
+                            ),
+                          ),
+                          Tab(
+                            child: Container(
+                              margin: EdgeInsets.only(right: 23),
+                              child: Text('Favourite'),
+                            ),
+                          ),
+                          Tab(
+                            child: Container(
+                              margin: EdgeInsets.only(right: 23),
+                              child: Text('Terpopuler'),
+                            ),
+                          )
+                        ]),
+                  ),
+                ),
+              ]))
             ],
           ),
         ),
